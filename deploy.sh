95275e17bc758fe1950d2d24194363f3f7d0fc27
NoelDj <noel.klp@gmail.com>
2023-12-20 02:18:21 +0100

    Update layout

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,6 @@
 git pull
 echo "Starting deployment"
 cd ~/ms_listings_web_application/ || exit
-docker pull registry.gitlab.com/noel100/ms_listings_web_application/djangoapp
-docker pull registry.gitlab.com/noel100/ms_listings_web_application/astroapp
-RTE=dev docker-compose down
-RTE=dev docker-compose up -d
+RTE=dev docker-compose pull
+RTE=dev docker-compose up -d --no-deps --build
 echo "Deployment completed"
