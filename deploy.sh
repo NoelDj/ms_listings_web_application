e3d1f97b33bd1f5c777f4ae885d698d28d7d7294
NoelDj <noel.klp@gmail.com>
2023-12-20 02:41:28 +0100

    Update index page

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-git pull
 echo "Starting deployment"
 cd ~/ms_listings_web_application/ || exit
-RTE=dev docker-compose pull
+RTE=dev docker-compose pull astroapp djangoapp
 RTE=dev docker-compose up -d --no-deps --build
 echo "Deployment completed"
