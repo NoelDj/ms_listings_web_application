42741d0e5465daa7eb77335d2b74a47678be33b4
NoelDj <noel.klp@gmail.com>
2023-12-20 01:59:56 +0100

    Update pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 git pull
-echo "starting deploying"
+echo "Starting deployment"
 cd ~/ms_listings_web_application/ || exit
+docker pull registry.gitlab.com/noel100/ms_listings_web_application/djangoapp
+docker pull registry.gitlab.com/noel100/ms_listings_web_application/astroapp
 RTE=dev docker-compose down
 RTE=dev docker-compose up -d
+echo "Deployment completed"
